f94965fb4b225c879f287b33faba8f4f76e4d1e5
Влад <trembling.of.the.strings@gmail.com>
2022-03-15 23:35:19 +0500

    deleted comments by default

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,18 +1,18 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
 
-# Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -55,7 +55,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 
-# Database
+
 
 DATABASES = {
     'default': {
@@ -65,7 +65,7 @@ DATABASES = {
 }
 
 
-# Password validation
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -83,7 +83,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
+
 
 LANGUAGE_CODE = 'en-us'
 
@@ -96,7 +96,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+
 
 STATIC_URL = '/static/'
 
