1e9246d981e3363fc615de4f1d5a94fe2112b227
John DoeAlexandra <aleksandra94@list.ru>
2022-03-20 09:22:46 +0300

    User model add

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,59 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+USER = 'user'
+MODERATOR = 'moderator'
+ADMIN = 'admin'
+CHOICES = (
+    (USER, 'Аутентифицированный пользователь'),
+    (MODERATOR, 'Модератор'),
+    (ADMIN, 'Администратор '),
+)
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=150,
+        unique=True
+    )
+    email = models.EmailField(
+        verbose_name='Email',
+        max_length=254,
+        unique=True
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+        blank=True
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150,
+        blank=True
+    )
+    bio = models.TextField(
+        verbose_name='О себе',
+        blank=True
+    )
+    role = models.CharField(
+        verbose_name='Пользовательская роль',
+        max_length=10,
+        choices=CHOICES,
+        default=USER
+    )
+
+    @property
+    def is_moderator(self):
+        return self.role == MODERATOR
+
+    @property
+    def is_admin(self):
+        return self.role == ADMIN
+
+    class Meta:
+        verbose_name_plural = 'Пользователи'
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.username
